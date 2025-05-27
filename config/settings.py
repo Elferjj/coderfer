ac73c1cde752349c4308ab262e0d2c4579f6d171
Juarez Fernando <ferchu303@hotmail.com>
2025-05-27 18:35:32 -0300

    Agregar gitignore y crear proyecto Django

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l%tmrbn$-ae%bh^2^mr7^st(1o22s4qj*rn%bt0s&5p8ma-o@0'
+SECRET_KEY = 'django-insecure-n=b_vk9%88*27i=o44ijqy3hv8ioyzk%j&d^xzgxy25d3_80al'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
